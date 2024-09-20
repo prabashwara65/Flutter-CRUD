57d2b16a5f744422e307c5317d56308056410108
prabashwara65 <prabashwara65@gmail.com>
2024-09-20 14:41:43 +0530

    design ui for home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/services/database_service.dart';
 
 class HomePage extends StatefulWidget{
   const HomePage({Key? key}) : super(key: key);
@@ -8,9 +9,26 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage>{
+
+  final DatabaseService _databaseService = DatabaseService();
   
   @override
   Widget build(BuildContext context){
-    return Scaffold();
+    return Scaffold(
+      appBar: _appBar(),
+    );
+    
+  }
+  
+  PreferredSizeWidget _appBar(){
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      title: const Text(
+        "Todo",
+        style: TextStyle(
+          color: Colors.white,
+        ),
+        ),
+    );
   }
 }
